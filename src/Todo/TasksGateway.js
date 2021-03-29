36902c4460ddd114856d3762ade84d0eb702cd8c
NKhoroshulia <43009571+NKhoroshulia@users.noreply.github.com>
2021-03-29 13:03:52 +0300

    Add files via upload

---
+++ src/Todo/TasksGateway.js
@@ -0,0 +1,54 @@
+const baseUrl = 'https://crudcrud.com/api/e2b58a8933bc440fa5272581fb9504ad/tasks';
+
+export const createTask = taskData => {
+    return fetch(baseUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;utc-8'
+        },
+        body: JSON.stringify(taskData),
+      }).then(response => {
+        if (!response.ok) {
+            throw new Error('Faild to create task');
+        }
+      });
+};
+
+export const fetchTasksList = () => {
+    return fetch(baseUrl)
+      .then(res => {
+        if(res.ok) {
+          return res.json();
+        }
+      })
+      .then(tasksList => 
+        tasksList.map(({ _id, ...task }) => ({ 
+          id: _id, 
+          ...task,
+        })),
+      );
+}
+
+export const updateTask = (taskId, taskData) => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json;utc-8'
+        },
+        body: JSON.stringify(taskData),
+      }).then(response => {
+        if (!response.ok) {
+            throw new Error('Faild to create task');
+        }
+      });
+}
+
+export const deleteTask = taskId => {
+    return fetch(`${baseUrl}/${taskId}`, {
+        method: 'DELETE' 
+      }).then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to delete task');
+        }
+      });
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './Todo/index.scss';
+import App from './Todo/App';
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
